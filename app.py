73667c45a905929b3389fcc7089f239651d71dc5
Chandan Kumar Nayak <107075048+MrProgrammerr@users.noreply.github.com>
2024-06-26 15:03:25 +0530

    new

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import google.generativeai as genai
 from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import urlparse, parse_qs
 
 load_dotenv()
 
@@ -14,10 +15,24 @@ prompt = """
     using maximum 500 words. The transcript text is : 
 """
 
-def get_transcript_from_yt(youtube_video_url):
+def extract_youtube_video_id(url):
+    parsed_url = urlparse(url)
+    if parsed_url.hostname in ['www.youtube.com', 'youtube.com']:
+        if parsed_url.path == '/watch':
+            video_id = parse_qs(parsed_url.query).get('v')
+            if video_id:
+                return video_id[0]
+        elif parsed_url.path.startswith('/embed/'):
+            return parsed_url.path.split('/')[2]
+        elif parsed_url.path.startswith('/v/'):
+            return parsed_url.path.split('/')[2]
+    elif parsed_url.hostname in ['youtu.be']:
+        return parsed_url.path[1:]
+    return None
+
+def get_transcript_from_yt(video_id):
     try:
-        video_id=youtube_video_url.split("=")[1]
-        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
+        transcript_text=YouTubeTranscriptApi.get_transcript(video_id, languages=('en',))
         transcript = ""
         for i in transcript_text:
             transcript += " " + i["text"]
@@ -33,28 +48,26 @@ def generate_summary(transcribed_text):
     response = model.generate_content(prompt+transcribed_text)
     return response.text
 
-text= get_transcript_from_yt("https://www.youtube.com/watch?v=HFfXvfFe9F8")
-print(generate_summary(text))
 
 st.title("Youtube Text Transcriber and Summarizer")
 yt_link = st.text_input("Enter Youtube Video URL : ")
+video_id = ""
 
 if yt_link:
-    video_id = yt_link.split("=")[1]
+    video_id = extract_youtube_video_id(yt_link)
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Transcribed Text"):
-    transcript_text=get_transcript_from_yt(yt_link)
-
+    transcript_text=get_transcript_from_yt(video_id)
     if transcript_text:
         st.markdown("## Transcript:")
         st.write(transcript_text)
 if st.button("Get Detailed Summary"):
-    transcript_text=get_transcript_from_yt(yt_link)
-
+    transcript_text=get_transcript_from_yt(video_id)
+    summary=generate_summary(transcript_text)
     if transcript_text:
-        summary=generate_summary(transcript_text)
         st.markdown("## Detailed Notes:")
         st.write(summary)
 
 # "conda activate venv/" :- in cmd
+# "python -m streamlit run app.py"
